a5c7e4dd493b408f5288aa05fa999fd5b07a994c
Tilo Reboucas <tilowr@gmail.com>
2020-05-29 22:31:59 -0300

    expose 3000

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,14 @@ const PORT = 3000;
 
 const app = express();
 
-console.log("app.set", app.set);
 app.set("port", PORT);
 
-console.log("app.get", app.get);
 app.get("/", (req: express.Request, res: express.Response) => {
     res.json({
         message: "Docker He's Alive!",
     });
 });
 
-console.log("app.listen", app.listen);
 app.listen(PORT, () => {
     console.log(`Aplicação escutando na porta ${PORT}`);
 });
